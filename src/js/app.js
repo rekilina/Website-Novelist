a41b045e32e115f3bec2a6ea4af488c31a288121
Irina Shebarshina <iv.shebarshina@gmail.com>
2022-11-15 15:25:44 +0400

    header actions

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
 import * as flsFunctions from "./modules/functions.js"
 
 flsFunctions.iswebp();
+flsFunctions.dropdownOther();
+flsFunctions.moveLiHeader();
+$(window).on("resize", flsFunctions.moveLiHeader);
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -16,3 +16,66 @@ export function iswebp() {
     })
 }
 
+var otherList = $('.header__nav-otherlist');
+var mainList = $('.header__nav-mainlist');
+
+export function dropdownOther() {
+    $('#otherlist').click(function() {
+        // let ChildrenCount = otherlist[0].childElementCount;
+        let ChildrenCount = otherList.children().length;
+        var r = $(':root');
+        let lineHeight = parseFloat(r.css("--otherlist-lineheight"));
+        let newHeight = ChildrenCount * lineHeight + 1;
+        r.css("--otherlist-height", String(newHeight)+"em");
+        otherList.toggleClass('visible');
+    })
+}
+
+// link is DOM Element not jq object
+function fromMaintoOther(link) {
+    if ($(link).parent().parent().hasClass('header__nav-mainlist')) {
+        let targetLi = $(link).parent()[0];
+        otherList[0].prepend(targetLi);
+    }
+}
+function fromOthertoMain(link) {
+    if (link.parentElement.parentElement.classList.contains('header__nav-otherlist')) {
+        let targetLi = link.parentElement;
+        $(targetLi).insertBefore($(".header__nav-li-other"));
+    }
+}
+
+export async function moveLiHeader() {
+    // console.log($("nav .header__nav-link")[7].parentElement);
+    let ww = window.innerWidth;
+    let mainListLen = mainList.children().length;
+    let allNavLinks = $("nav .header__nav-link");
+    // console.log(mainListLen);
+    if (ww < 1001) {
+        for (let link of allNavLinks) {
+            if (link.textContent.trim() == 'Интервью') {
+                fromMaintoOther(link);
+            }
+        }
+    } else {
+        for (let link of allNavLinks) {
+            if (link.textContent.trim() == 'Интервью') {
+                fromOthertoMain(link);
+            }
+        }
+    }
+    if (ww < 800) {
+        for (let link of allNavLinks) {
+            if (link.textContent.trim() == 'Рецензии') {
+                fromMaintoOther(link);
+            }
+        }
+    } else {
+        for (let link of allNavLinks) {
+            if (link.textContent.trim() == 'Рецензии') {
+                fromOthertoMain(link);
+            }
+        }
+    }
+    
+}
